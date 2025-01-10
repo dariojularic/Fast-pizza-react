abdab2a5bc3c2fa93a55a53a6b9c5fb12799c242
Dario Jularic <dariojularic0@gmail.com>
2025-01-10 10:50:25 +0100

    Add style

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,16 +1,18 @@
+import "./LandingPage.css"
+
 const LandingPage = () => {
   return (
-    <>
+    <div className="landing-page">
       <div className="headers">
         <h1>The best pizza</h1>
         <h2>Straight out of the oven, straight to you</h2>
       </div>
 
-      <div>
+      <div className="user-input-container">
         <h3>Welcome! Please start by telling us your name: </h3>
-        <input type="text" placeholder="Your full name" />
+        <input type="text" placeholder="Your full name" name="username"  />
       </div>
-    </>
+    </div>
   );
 };
 
