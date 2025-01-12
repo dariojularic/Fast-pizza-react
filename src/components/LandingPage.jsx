73486fadd2162019ab6abdb88c3f239b0c07e545
Dario Jularic <dariojularic0@gmail.com>
2025-01-12 13:03:53 +0100

    Add style

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -4,13 +4,13 @@ const LandingPage = () => {
   return (
     <div className="landing-page">
       <div className="headers">
-        <h1>The best pizza</h1>
-        <h2>Straight out of the oven, straight to you</h2>
+        <h1>The best pizza <br></br><span className="header-span" >Straight out of the oven, straight to you</span></h1>
+        {/* <h2>Straight out of the oven, straight to you</h2> */}
       </div>
 
       <div className="user-input-container">
-        <h3>Welcome! Please start by telling us your name: </h3>
-        <input type="text" placeholder="Your full name" name="username"  />
+        <h3>👋 Welcome! Please start by telling us your name: </h3>
+        <input className="username-input" type="text" placeholder="Your full name" name="username"  />
       </div>
     </div>
   );
