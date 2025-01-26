abf096583d36ee9faf9aab751727d00fab01e62e
Dario Jularic <dariojularic0@gmail.com>
2025-01-26 14:44:43 +0100

    Refactor code

--- src/layouts/pizzaCard/PizzaCard.jsx
+++ src/layouts/pizzaCard/PizzaCard.jsx
@@ -1,21 +1,21 @@
-import Button from "../../components/Button";
+// import Button from "../../components/Button";
 
-const PizzaCard = ({ name, ingredients, soldOut, price, image }) => {
-  return (
-    <li>
-      <div className="pizza-info">
-        <img src={image} alt="" />
-        <h4 className="pizza-name">{name}</h4>
-        <p className="pizza-ingredients">
-          {ingredients.map((ingredient) => {
-            return ingredient;
-          })}
-        </p>
-        <p className="pizza-price">{price}</p>
-      </div>
-      <Button />
-    </li>
-  );
-};
+// const PizzaCard = ({ name, ingredients, soldOut, price, image }) => {
+//   return (
+//     <li>
+//       <div className="pizza-info">
+//         <img src={image} alt="" />
+//         <h4 className="pizza-name">{name}</h4>
+//         <p className="pizza-ingredients">
+//           {ingredients.map((ingredient) => {
+//             return ingredient;
+//           })}
+//         </p>
+//         <p className="pizza-price">{price}</p>
+//       </div>
+//       <Button />
+//     </li>
+//   );
+// };
 
-export default PizzaCard;
+// export default PizzaCard;
