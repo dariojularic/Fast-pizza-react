67c8df252e592301b76dadefc9d4508f99aeca1d
Dario Jularic <dariojularic0@gmail.com>
2025-01-28 13:35:58 +0100

    Refactor code

--- src/pages/menu/Menu.jsx
+++ src/pages/menu/Menu.jsx
@@ -1,19 +1,40 @@
 import PizzaCard from "../../layouts/pizzaCard/PizzaCard";
+import { useState, useEffect } from "react";
 
 const Menu = () => {
+  const [pizzaMenu, setPizzaMenu] = useState([]);
   const url = "https://react-fast-pizza-api.onrender.com/api/menu";
 
-  async function fetchData() {
-    try {
-      const response = await fetch(url);
-      if (response.status !== 200) throw new Error("Something went wrong!");
-      const data = await response.json();
-      console.log(data.data);
-      return data;
-    } catch (error) {
-      alert(error);
-    }
-  }
+  // async function fetchData() {
+  //   try {
+  //     const response = await fetch(url);
+  //     if (response.status !== 200) throw new Error("Something went wrong!");
+  //     const data = await response.json();
+  //     console.log(data.data);
+  //     setPizzaMenu(data);
+  //     return data;
+  //   } catch (error) {
+  //     alert(error);
+  //   }
+  // }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        // console.log(data.data);
+        setPizzaMenu(data.data);
+        // console.log(pizzaMenu);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  // console.log(pizzaMenu);
 
   // fetchData();
 
@@ -41,22 +62,24 @@ const Menu = () => {
     <>
       <h1>Menu Page</h1>
       <ul>
-        {() => {
-          fetchData().then((data) => {
-            data.data.map((pizza) => {
-              return (
-                <PizzaCard
-                  key={pizza.id}
-                  name={pizza.name}
-                  ingredients={pizza.ingredients}
-                  soldOut={pizza.soldOut}
-                  image={pizza.imageUrl}
-                  price={pizza.unitPrice}
-                />
-              );
-            });
+        {pizzaMenu.map((pizza) => {
+          console.log(pizzaMenu);
+          return (
+            <PizzaCard
+              key={pizza.id}
+              name={pizza.name}
+              ingredients={pizza.ingredients}
+              soldOut={pizza.soldOut}
+              image={pizza.imageUrl}
+              price={pizza.unitPrice}
+            />
+          );
+        })}
+        {/* {fetchData().then((data) => {
+          console.log(pizzaMenu);
+          return data.data.map((pizza) => {
           });
-        }}
+        })} */}
       </ul>
     </>
   );
