df4822233eb46929b244a8c88a7c70a3a1d4a3a9
Dario Jularic <dariojularic0@gmail.com>
2025-02-03 09:35:05 +0100

    Create footer

--- src/layouts/SharedLayout.jsx
+++ src/layouts/SharedLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./navbar/Navbar";
+import Footer from "./footer/Footer";
 
 const SharedLayout = () => {
   return (
     <>
       <Navbar />
       <Outlet />
+      <Footer/>
     </>
   );
 };
---
+++ src/layouts/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css"
+
+function Footer() {
+  return(
+    <footer>
+      <div className="cart-info">gaga</div>
+      <div className="cart-button">ssss</div>
+    </footer>
+  )
+}
+
+export default Footer;
