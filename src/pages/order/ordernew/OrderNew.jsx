a669f1aade970782a5670782851271ba79587d38
Dario Jularic <dariojularic0@gmail.com>
2025-02-05 22:00:37 +0100

    Refactor code

--- src/pages/order/ordernew/OrderNew.jsx
+++ src/pages/order/ordernew/OrderNew.jsx
@@ -4,10 +4,30 @@ import { Link } from "react-router-dom";
 import Button from "#components/Button";
 
 function OrderNew() {
+
+  const handleSubmit = (event) => {
+    // event.preventDefault()
+
+    fetch("https://react-fast-pizza-api.onrender.com/api/order", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+    })
+      .then(response => response.json())
+      .then(responseData => {
+        console.log("Success:", responseData)
+      })
+      .catch(error => {
+        console.error("Error", error)
+      })
+  }
+
+
   return (
     <div className="order-new-container">
       <h2>Ready to order? Let's go!</h2>
-      <form className="order-form" method="POST" action="https://react-fast-pizza-api.onrender.com/api/order">
+      <form onSubmit={() => handleSubmit()} className="order-form" method="POST" action="https://react-fast-pizza-api.onrender.com/api/order">
         <OrderNewElement value="First Name" name="first-name" />
         <OrderNewElement value="Phone Number" name="phone-number" />
         <OrderNewElement value="Address" name="address" />
@@ -16,7 +36,7 @@ function OrderNew() {
           <label htmlFor="priority">Want to give your order priority?</label>
         </div>
         {/* <Link> */}
-          <Button type="submit" value={`ORDER NOW FOR €`} style="btn" />
+          <Button type="button" value={`ORDER NOW FOR €`} style="btn" handler={() => handleSubmit()} />
         {/* </Link> */}
       </form>
     </div>
