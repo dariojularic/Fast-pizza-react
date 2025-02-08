bf109d5014684892b30ecd0b21dc2cfe7e33369b
Dario Jularic <dariojularic0@gmail.com>
2025-02-08 13:46:56 +0100

    Refactor code

--- src/pages/order/orderid/OrderId.jsx
+++ src/pages/order/orderid/OrderId.jsx
@@ -1,31 +1,70 @@
-import "./OrderId.css"
-
-// pogledat hook useParams iz react routera
-// samo useState
+import { useEffect, useState } from "react";
+import "./OrderId.css";
+import { useParams } from "react-router-dom";
+import Loader from "#layouts/loader/Loader.jsx";
+import formatDistanceToNow from "date-fns"
 
 function OrderId() {
+  const [orderStatus, setOrderStatus] = useState({});
+  const [orderStatusReady, setOrderStatusReady] = useState(false);
+  const params = useParams();
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(
+          `https://react-fast-pizza-api.onrender.com/api/order/${params.id}`
+        );
+        const data = await response.json();
+        setOrderStatus(data.data);
+        setOrderStatusReady(true);
+        console.log(data.data);
+      } catch (err) {
+        console.error("Error fetching data:", err);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  console.log(orderStatus);
+
+  if (orderStatusReady === false) return <Loader />;
+
   return (
     <div className="order-id">
-      <h1>order ID</h1>
       <div className="order-status">
-        <p>Order *** status</p>
-        <p>Preparing status</p>
+        <h3>Order #{orderStatus.id} status</h3>
+        <div className="priority-status-container">
+          {orderStatus.priority && <p className="priority-paragraph">PRIORITY</p>}
+          <p className="status-paragraph">{orderStatus.status}</p>
+        </div>
       </div>
 
       <div className="delivery-container">
         <p>Only 20 minutes left 😀</p>
-        <p>Estimated delivery:</p>
+        <p>Estimated delivery: {orderStatus.estimatedDelivery}</p>
       </div>
 
       <div className="order-container">
-        <div className="pizza-container">
-          <p className="num-of-pizzas"></p>
-          <p className="pizza-price"></p>
-        </div>
+        {orderStatus.cart.map((pizza) => {
+          return (
+            <>
+              <div key={pizza.id} className="pizza-container">
+                <p className="num-of-pizzas">
+                  {pizza.quantity} x {pizza.name}
+                </p>
+                <p className="pizza-price">€{pizza.totalPrice.toFixed(2)}</p>
+              </div>
+              <hr />
+            </>
+          );
+        })}
       </div>
 
       <div className="price-container">
         <p>Price pizza: €20</p>
+        <p>Price priority: €20</p>
         <p>To pay on delivery: €20</p>
       </div>
     </div>
